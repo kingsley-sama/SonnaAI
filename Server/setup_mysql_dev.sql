69a5b107ddb86afa0922703ed3903fe795864b5d
Kingsley Okpo <kngsley2018@gmail.com>
2024-10-31 03:07:24 +0100

    creates the entry file for my node js server

--- Server/setup_mysql_dev.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS sonna_ai_dev_db;
-CREATE USER IF NOT EXISTS 'kingsley'@'localhost' IDENTIFIED BY 'sonna_ai_dev_pwd';
-GRANT ALL PRIVILEGES ON `sonna_ai_dev_db`.* TO 'kingsley'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'kingsley'@'localhost';
-FLUSH PRIVILEGES;
